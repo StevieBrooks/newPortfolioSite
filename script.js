3d5d37eda803b20ff2c3cc412817d2b7ce237d78
Steven Brooks <brookscodeblog@gmail.com>
2023-03-27 14:38:18 +0700

    Burger menu problem sorted

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const burgerMenu = document.querySelector('.burger-menu');
 const burgerOptions = document.querySelector('.burger-menu-options');
+const overlay = document.querySelector('.overlay');
 
 burgerMenu.addEventListener('click', () => {
+    burgerMenu.classList.toggle("active");
     burgerOptions.classList.toggle("active");
+    overlay.classList.toggle("active");
 })
